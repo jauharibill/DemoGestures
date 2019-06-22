83b18cbb71b901ff0bddf22e5172a9a75edb371d
Idris <idrisnovandi@gmail.com>
2019-06-22 21:09:17 +0700

    Initial Commit

---
+++ DemoGestures/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DemoGestures
+//
+//  Created by Idris on 22/06/19.
+//  Copyright © 2019 IdrisLabs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
